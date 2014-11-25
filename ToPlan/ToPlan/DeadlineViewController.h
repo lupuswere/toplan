45e126c0b442601d7309ebd0e02621e8ef1bd6cf
Lifei Li <llf17@uw.edu>
2014-11-24 22:02:25 -0800

    deadlineviewcontroller created

---
+++ ToPlan/ToPlan/DeadlineViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeadlineViewController.h
+//  ToPlan
+//
+//  Created by Lifei Li on 14/11/24.
+//  Copyright (c) 2014年 Other Candidates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeadlineViewController : UIViewController
+
+@end
---
+++ ToPlan/ToPlan/DeadlineViewController.m
@@ -0,0 +1,13 @@
+//
+//  DeadlineViewController.m
+//  ToPlan
+//
+//  Created by Lifei Li on 14/11/24.
+//  Copyright (c) 2014年 Other Candidates. All rights reserved.
+//
+
+#import "DeadlineViewController.h"
+
+@implementation DeadlineViewController
+
+@end
